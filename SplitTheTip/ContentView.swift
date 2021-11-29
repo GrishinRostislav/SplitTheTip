bbbc78106c464b458b39a5bc7331ce921ace111d
Rostislav Grishin <grishin.r@outlook.com>
2021-11-29 13:39:46 +0200

    Initial Commit

---
+++ SplitTheTip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SplitTheTip
+//
+//  Created by Rostislav Grishin on 29.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplitTheTip/SplitTheTipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitTheTipApp.swift
+//  SplitTheTip
+//
+//  Created by Rostislav Grishin on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SplitTheTipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
